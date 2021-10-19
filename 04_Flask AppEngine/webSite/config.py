7ea1316bf2ecbe8bcf7803b2bc57eb19443beedd
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-19 23:33:01 +0800

    Delete 04_Flask AppEngine directory

--- 04_Flask AppEngine/webSite/config.py
+++
@@ -1,9 +0,0 @@
-class Config():
-    def __init__(self) :
-        self.channelAccessToken = '<your_channelAccessToken>'
-        # ID 不要改
-        self.site = {
-            "oVAFVkOopfRFXhv21bov" : "忠孝一店",
-            "Bs58J0x6GIsO7usO8NWi" : "仁愛二店",
-            "vMNDFZX6ru5O821bVVDQ" : "信義三店"
-        }
--- 04_Flask AppEngine/webhook/chatBotConfig.py
+++
@@ -1,4 +0,0 @@
-channel_secret = '<your_channelSecret>'
-channel_access_token = '<your_channelAccessToken>'
-richmenu_json_path = "richmenu.json"
-richmenu_png_path = "richmenu.png"
