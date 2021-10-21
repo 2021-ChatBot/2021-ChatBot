f7ee4f199f822c47d5445d329f9f29a77ef9c9f1
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-21 21:53:50 +0800

    Update firebaseApi.py

--- 08_期中專案/website/firebaseApi.py
+++ 08_期中專案/website/firebaseApi.py
@@ -2,74 +2,102 @@ from firebase_admin import credentials, firestore, initialize_app
 from config import Config
 
 config = Config()
-
-
 cred = credentials.Certificate(config.firebase()['keyFile'])
 initialize_app(cred, {'storageBucket': config.firebase()['storageBucket']})
 
+
 class Firebase:
-    def __init__(self, companyId = config.companyId):
+    
+    def __init__(self, companyId=config.companyId):
         self.__companyId = companyId
         self.__firestore = firestore.client()
-        self.__myDb = self.__firestore.collection(config.firebase()['projectName']).document(config.firebase()['dbName'])
-        self.__sitesCollection = self.__myDb.collection("companies").document(self.__companyId).collection(config.firebase()['siteTable'])
+        self.__myDb = self.__firestore.collection(config.firebase()['projectName']).document(
+            config.firebase()['dbName'])
+        self.__sitesCollection = self.__myDb.collection("companies").document(self.__companyId).collection(
+            config.firebase()['siteTable'])
         self.__membersCollection = self.__myDb.collection(config.firebase()['memberTable'])
 
+
     # --------members--------------
-    def getMemberData(self, memberId = None):
-        if memberId == None :
+    def getMemberData(self, memberId=None, companyId=None):
+        if memberId == None and companyId == None:
+            # get all member data from members
             membersData = list(doc._data for doc in self.__membersCollection.stream())
-            return membersData #list
-        else :
+            return membersData  # list
+        elif companyId != None:
+            membersData = []
+            try:
+                for member in self.__myDb.collection("companies").document(companyId).collection(
+                        config.firebase()['memberTable']).stream():
+                    memberData = self.__membersCollection.document(member.id).get().to_dict()
+                    membersData.append(memberData)
+            except:
+                pass
+            return membersData  # list
+        else:
+            # get single member data from members
             memberData = self.__membersCollection.document(memberId).get().to_dict()
-            return memberData #dict
-    def getMembersDataOfCompany(self):
-        membersData = []
-        try:
-            for member in self.__myDb.collection("companies").document(self.__companyId).collection(config.firebase()['memberTable']).stream():
-                memberData = self.__membersCollection.document(member.id).get().to_dict()
-                membersData.append(memberData)
-        except:
-            pass
-        return membersData #list
+            return memberData  # dict
+
+
     def putMemberData(self, memberData):
         memberId = memberData['id']
         self.__membersCollection.document(memberId).update(memberData)
 
+
     # --------sites----------------
-    def getSiteData(self,siteId = None):
+    def getSiteData(self, siteId=None, companyId=None):
         if siteId == None:
-            sitesData = list(doc._data for doc in self.__sitesCollection.stream())
-            return sitesData #list
+            sitesData = list(doc._data for doc in self.__myDb.collection("companies").document(companyId).collection(config.firebase()['siteTable']).stream())
+            return sitesData  # list
         else:
-            siteData = self.__sitesCollection.document(siteId).get().to_dict()
-            return siteData #dict
+            siteData = self.__myDb.collection("companies").document(companyId).collection(config.firebase()['siteTable']).document(siteId).get().to_dict()
+            return siteData  # dict
+
+
     # --------footprints-----------
-    def getFootprintsData(self, memberId=None, siteId=None):
-        if memberId != None :
-            footprintsData = list(doc._data for doc in self.__membersCollection.document(memberId).collection('footprints').stream())
-        elif siteId != None : 
-            footprintsData = list(doc._data for doc in self.__sitesCollection.document(siteId).collection('footprints').stream())
-        return footprintsData #list
+    def getFootprintsData(self, memberId=None, siteId=None, companyId=None):
+        footprintsData = []
+        if memberId != None:
+            footprintsData = list(
+                doc._data for doc in self.__membersCollection.document(memberId).collection('footprints').stream())
+        elif siteId != None and companyId is not None:
+            footprintsData = list(
+                doc._data for doc in self.__myDb.collection("companies").document(companyId).collection(config.firebase()['siteTable']).document(siteId).collection('footprints').stream())
+        return footprintsData  # list
+
 
     def postFootprint(self, footPrintModel):
         memberId = footPrintModel['memberId']
         siteId = footPrintModel['siteId']
-        response = self.__sitesCollection.document(siteId).collection('footprints').add(footPrintModel)[1]
-        self.__sitesCollection.document(siteId).collection('footprints').document(response.id).update({'id': response.id})
-        self.__membersCollection.document(memberId).collection('footprints').document(response.id).set(footPrintModel)
-        self.__membersCollection.document(memberId).collection('footprints').document(response.id).update({'id': response.id})
-        return response.id
-    # --------companies------------ 
+        footprintId = self.__sitesCollection.document(siteId).collection('footprints').add(footPrintModel)[1].id
+        self.__sitesCollection.document(siteId).collection('footprints').document(footprintId).update({'id': footprintId})
+        footPrintModel['id'] = footprintId
+        self.__membersCollection.document(memberId).collection('footprints').document(footprintId).set(footPrintModel)
+        return footprintId
 
-    def getCompaniesData(self):
-        companyIdOfName = {}
-        siteIdOfName = {}
-        for company in self.__myDb.collection("companies").stream():
-            companyIdOfName[company.id] = company._data['name']
-            for site in self.__myDb.collection("companies").document(company.id).collection(config.firebase()['siteTable']).stream():
+
+    # --------companies------------
+    def getCompaniesData(self, companyId=None):
+        companyDataList = []
+        if companyId is None:
+            for company in self.__myDb.collection("companies").stream():
+                siteIdOfName = {}
+                for site in self.__myDb.collection("companies").document(company.id).collection(config.firebase()['siteTable']).stream():
+                    siteIdOfName[site.id] = site._data['name']
+                companyDataList.append({"id": company.id, "name": company._data['name'], "sites": siteIdOfName})
+            return companyDataList
+        elif type(companyId) is str:
+            company = self.__myDb.collection("companies").document(companyId)
+
+            siteIdOfName = {}
+            for site in company.collection(config.firebase()['siteTable']).stream():
                 siteIdOfName[site.id] = site._data['name']
-        return companyIdOfName, siteIdOfName #dict, dict
+            return {"id": companyId, "name": company.get().to_dict()['name'], "sites": siteIdOfName}
+        else:
+            # empty dictionary
+            return companyDataList
+            # companyDataList -> [{"id" : "companyId" , "name" : "companyName", "sites" : {"siteId" : "siteName"}}]
         
     
 
