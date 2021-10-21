e1cc8e5b6bd94788fded59b5bd06b58fc3d600b8
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-21 21:50:52 +0800

    Update transmission.py

--- 08_期中專案/website/transmission.py
+++ 08_期中專案/website/transmission.py
@@ -5,21 +5,33 @@ from firebaseApi import Firebase
 
 class TransmissionTracker:
     def __init__(self, inputData):
-        self.__memberId = inputData["memberId"]
+        self.__companyId = inputData["companyId"]
+        self.__siteId = inputData["siteId"]
         self.__confirmedTime = inputData["confirmedTime"]
         self.__spreadStrength = inputData["spreadStrength"]
         self.__firebaseApi = Firebase()
         self.eventId = str(uuid.uuid4())
         self.infectedList = []
+        memberId = ""
 
-        # run algorithm
-        self.checkFootprints(self.__memberId, self.__confirmedTime, self.__spreadStrength)
+        site_visitList = self.__firebaseApi.getFootprintsData(siteId=self.__siteId, companyId=self.__companyId)
 
-        # clean duplicate infected data
-        self.infectedList = sorted(pd.DataFrame(self.infectedList).drop_duplicates().to_dict('records'),
-                                   key=lambda k: k['timestamp'])
+        site_visitList = sorted(site_visitList, key=lambda k: k['timestamp'])
 
-    def checkFootprints(self, memberId, timestamp, spreadStrength):
+        for i in range(len(site_visitList)):
+            if site_visitList[i]['timestamp'] >= self.__confirmedTime:
+                memberId = site_visitList[i]['memberId']
+                break
+
+        if memberId != "":
+            # run algorithm
+            self.checkFootprints(memberId, self.__confirmedTime, self.__spreadStrength, self.__companyId)
+
+            # clean duplicate infected data
+            self.infectedList = sorted(pd.DataFrame(self.infectedList).drop_duplicates().to_dict('records'),
+                                       key=lambda k: k['timestamp'])
+
+    def checkFootprints(self, memberId, timestamp, spreadStrength, companyId):
         infectedMember = {}
         infected_footprint = ""
         siteId = ""
@@ -41,12 +53,11 @@ class TransmissionTracker:
                 infectedMember["siteId"] = Memberfootprints[i]["siteId"]
                 infectedMember["companyId"] = Memberfootprints[i]["companyId"]
                 infectedMember["name"] = MemberInfo["name"]
-                infectedMember["lineId"] = MemberInfo["lineId"]
                 break
 
         # ----------------------------調出商店足跡列表----------------------------
         try:
-            site_visitList = self.__firebaseApi.getFootprintsData(siteId=siteId)
+            site_visitList = self.__firebaseApi.getFootprintsData(siteId=siteId, companyId=companyId)
         except Exception:
             site_visitList = []
         site_visitList = sorted(site_visitList, key=lambda k: k['timestamp'])
@@ -62,17 +73,20 @@ class TransmissionTracker:
             try:  # 往下追蹤成員足跡到底為止
                 return self.checkFootprints(memberId=Memberfootprints[memberIndex + 1]['memberId'],
                                             timestamp=Memberfootprints[memberIndex + 1]["timestamp"],
-                                            spreadStrength=self.__spreadStrength)
+                                            spreadStrength=self.__spreadStrength,
+                                            companyId=Memberfootprints[memberIndex + 1]['companyId'])
             except IndexError:
                 return None
         else:
             try:  # 先追蹤商店足跡, 再繼續往下追蹤成員足跡
                 return self.checkFootprints(memberId=site_visitList[siteIndex + 1]['memberId'],
                                             timestamp=site_visitList[siteIndex + 1]["timestamp"],
-                                            spreadStrength=spreadStrength - 1), \
+                                            spreadStrength=spreadStrength - 1,
+                                            companyId=Memberfootprints[memberIndex + 1]['companyId']), \
                        self.checkFootprints(memberId=Memberfootprints[memberIndex + 1]['memberId'],
                                             timestamp=Memberfootprints[memberIndex + 1]["timestamp"],
-                                            spreadStrength=self.__spreadStrength - 1)
+                                            spreadStrength=self.__spreadStrength - 1,
+                                            companyId=Memberfootprints[memberIndex + 1]['companyId'])
             except IndexError:
                 return None
 
@@ -80,12 +94,24 @@ class TransmissionTracker:
         return {
             "event": {
                 "eventId": self.eventId,
-                "infectedMember": self.__memberId,
-                "amount": len(set(pd.DataFrame(self.infectedList)["memberId"].drop_duplicates())) - 1,
+                "infectedSites": len(set(pd.DataFrame(self.infectedList)["siteId"].drop_duplicates())),
+                "amount": len(set(pd.DataFrame(self.infectedList)["memberId"].drop_duplicates())),
                 "strength": self.__spreadStrength,
-                "infectedTime": self.__confirmedTime,
+                "timestamp": self.__confirmedTime,
+                "companyName": self.__companyId,
+                "infectedSiteName": self.__firebaseApi.getSiteData(siteId=self.__siteId, companyId=self.__companyId)["name"],
+                "infectedfootprints": len(self.infectedList)
             }
         }
 
     def getResult(self) -> list:
-        return self.infectedList
+        # infectedList = [
+        #   {
+        #       "companyId" : "string",
+        #       "siteId" : "string",
+        #       "memberId" : "string",
+        #       "name" : "string",
+        #       "timestamp" : "int,
+        #   }
+        # ]
+        return sorted(self.infectedList, key=lambda k: (k['name'], k['timestamp']))
