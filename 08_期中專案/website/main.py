83dd70fa2915001bfdab53888bf7de51cdb72e45
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-21 21:48:20 +0800

    Update main.py

--- 08_期中專案/website/main.py
+++ 08_期中專案/website/main.py
@@ -1,6 +1,6 @@
 import os
 import re
-from flask import Flask, request, render_template, jsonify, redirect
+from flask import Flask, request, render_template, jsonify
 import json
 import time
 from datetime import datetime
@@ -24,7 +24,7 @@ def myData():
     memberId = request.values.get('memberId')
 
     if request.method == 'GET':
-        memberData = firebase.getMemberData(memberId)
+        memberData = firebase.getMemberData(memberId=memberId)
         title = "我的個資"
         return render_template('myData.html', memberData=memberData, title=title)
 
@@ -34,17 +34,16 @@ def myData():
             'role': request.values['role']
         }
         firebase.putMemberData(setUpData_dict)
-        memberData = firebase.getMemberData(memberId)
-        title = "角色設定成功"
-        return render_template('setUpMyData.html', memberData=memberData, title=title)
+        memberData = firebase.getMemberData(memberId=memberId)
+        return render_template('setUpMyData.html', memberData=memberData, title="角色設定成功")
 
 
 # ----------------------掃碼---------------------------------------------
 @app.route("/scanQrCode", methods=['GET'])
 def scanQrCode():
     memberId = request.values.get('memberId')
-    title = "實聯掃碼"
-    return render_template('scanQrCode.html', memberId=memberId, title=title)
+    return render_template('scanQrCode.html', memberId=memberId, title="實聯掃碼")
+
 
 # ----------------------寫入掃碼足跡紀錄-----------------------------------
 @app.route("/recordFootprint", methods=['POST'])
@@ -55,23 +54,22 @@ def recordFootprint():
         siteId = siteIdRegex.findall(footprintData['siteInfo'])[0]
     except:
         return jsonify('這不是實聯制QRcode')
-        
+
     memberId = footprintData['memberId']
     importTime = int(time.time() + 28800)
+    for sitesData in firebase.getCompaniesData():
+        if siteId in sitesData['sites']:
+            siteData = sitesData['sites']
+            break
 
-    siteData = firebase.getSiteData(siteId)
-    memberData = firebase.getMemberData(memberId)
+    memberData = firebase.getMemberData(memberId=memberId)
     if siteData != None:
         notificationModel = {
-            "receiverLineIdList": memberData['lineId'],
-            "messages": [
-                {
-                    "messageType": "textTemplate",
-                    "content": "掃碼成功\n"
-                               f"商店: {siteData['name']}\n"
-                               f"時間: {str(datetime.utcfromtimestamp(importTime).strftime('%Y-%m-%d %H:%M:%S'))}\n"
-                    }
-            ]
+            "lineId": memberData['lineId'],
+            "messageType": "textTemplate",
+            "content": "掃碼成功\n"
+                       f"商店: {siteData[siteId]}\n"
+                       f"時間: {str(datetime.utcfromtimestamp(importTime).strftime('%Y-%m-%d %H:%M:%S'))}"
         }
         notificationThread = threading.Thread(target=push.pushMessage, args=(notificationModel,))
         notificationThread.start()
@@ -84,12 +82,12 @@ def recordFootprint():
             'companyId': config.companyId,
             'timestamp': importTime
         }
-        footPrintId = firebase.postFootprint(footprintModel_dict)
+        footprintId = firebase.postFootprint(footprintModel_dict)
 
         # - pubsub to bigquery
         bigquery_dict = {
             'footprint': {
-                'id': footPrintId,
+                'id': footprintId,
                 'member': footprintModel_dict['memberId'],
                 'site': footprintModel_dict['siteId'],
                 'timestamp': importTime
@@ -99,7 +97,7 @@ def recordFootprint():
         bigqueryThread = threading.Thread(target=edgePub, args=(bigquery_dict,))
         bigqueryThread.start()
 
-        return jsonify(siteData['name'] + '  到店掃碼成功')
+        return jsonify(siteData[siteId] + '  到店掃碼成功')
     else:
         return jsonify('這不是實聯制QRcode')
 
@@ -108,48 +106,79 @@ def recordFootprint():
 @app.route("/myFootprint", methods=['GET'])
 def myFootprint():
     memberId = request.values.get('memberId')
-    companyIdOfName, siteIdOfName = firebase.getCompaniesData()
+    companyDataList = firebase.getCompaniesData()
     footprintsDataOfUser = firebase.getFootprintsData(memberId=memberId)
     footprintsDataOfUser.sort(key=lambda k: (k.get('timestamp', 0)))
 
     footPrintsData_list = []
     for footprintData in footprintsDataOfUser:
         if footprintData != None:
-            siteId = footprintData['siteId']
-            companyId = footprintData['companyId']
-            footprintData['timestamp'] = str(
-                datetime.datetime.utcfromtimestamp(footprintData['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
+            for i in range(len(companyDataList)):
+                if footprintData['siteId'] in companyDataList[i]['sites'].keys():
+                    siteId = footprintData['siteId']
 
-            footPrintsData_list.append(
-                {
-                    'companyName': companyIdOfName[companyId],
-                    'siteName': siteIdOfName[siteId],
-                    'timestamp': footprintData['timestamp']
-                }
-            )
-    title = "足跡列表"
-    return render_template('footprintList.html', footPrintsData_list=footPrintsData_list, title=title)
+                    footprintData['timestamp'] = str(
+                        datetime.utcfromtimestamp(footprintData['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
 
-
-# ----------------------------data studio-----------------------------
-@app.route("/epidemicReport", methods=['GET'])
-def getReport():
-    return render_template('dataStudio.html')
+                    footPrintsData_list.append(
+                        {
+                            'companyName': companyDataList[i]['name'],
+                            'siteName': companyDataList[i]['sites'][siteId],
+                            'timestamp': footprintData['timestamp']
+                        }
+                    )
+    return render_template('footprintList.html', footPrintsData_list=footPrintsData_list, title="足跡列表")
 
 
-# ----------------------------疫調通報組態設定-----------------------------
+# ----------------------------組織管理-----------------------------
+@app.route("/organizationManagement", methods=['GET'])
+def organizationManagement():
+    companyDataList = firebase.getCompaniesData()
+    companyIdOfName = {}
+    for company in companyDataList:
+        companyIdOfName[company["id"]] = company["name"]
+    companyData = {}
+    sitesData = firebase.getSiteData(companyId=config.companyId)
+    companyData["sitesData"] = sitesData
+    membersData = firebase.getMemberData(companyId=config.companyId)
+    companyData["membersData"] = membersData
+    return render_template('organizationManagement.html', companyData=companyData, companyIdOfName=companyIdOfName, companyId=config.companyId, title='組織管理')
+
+
+# -----------------------getOtherCompanyData----------------------
+@app.route("/getCompanyData", methods=['POST'])
+def getCompanyData():
+    companyId = request.get_json()["companyId"]
+    companyData = {}
+    sitesData = firebase.getSiteData(companyId=companyId)
+    companyData["sitesData"] = sitesData
+    membersData = firebase.getMemberData(companyId=companyId)
+    companyData["membersData"] = membersData
+    return companyData
+    # companyData->{"sitesData":[], "membersData":[]}
+
+
+# ----------------------------疫情調查設定-----------------------------
 @app.route("/epidemicSurvey", methods=['GET'])
 def epidemicSurvey():
-    membersData = firebase.getMemberData()
-    return render_template('epidemicSurvey.html', membersData=membersData, title="疫情調查")
+    companyDataList = firebase.getCompaniesData()
+    sitesData = {}
+    for company in companyDataList:
+        sitesids = {}
+        sitesids.update(company["sites"])
+        for siteId in company["sites"].keys():
+            sitesData.update({f"{company['id']}-{siteId}": f'{company["name"]} {sitesids[siteId]}'})
+
+    return render_template('epidemicSurvey.html', sitesData=sitesData, title="疫情調查")
 
 
-# ----------------------------疫調通報結果-----------------------------
+# ----------------------------疫情調查結果-----------------------------
 @app.route("/checkFootprintFlow", methods=['POST'])
 def checkFootprintFlow():
-    memberData = firebase.getMemberData(request.values['memberId'])
+    companyId, siteId = request.values['siteId'].split('-')
     inputData = {
-        'memberId': request.values['memberId'],
+        'companyId': companyId,
+        'siteId': siteId,
         'spreadStrength': int(request.values['spreadStrength']),
         'confirmedTime': time.mktime(
             datetime.strptime(request.values['confirmedTime'], "%Y-%m-%dT%H:%M:%S").timetuple()) + 28800,
@@ -169,54 +198,44 @@ def checkFootprintFlow():
             'spreadStrength': str(request.values['spreadStrength']),
             'eventId': eventId,
             "timestamp": str(request.values['confirmedTime']).replace("T", " "),
-            "name": memberData['name']
+            "name": firebase.getSiteData(siteId=siteId)["name"]
         }
         return render_template('quarantineList.html', infectedList=infectedList, result=result, title="疫情調查")
 
-    companyIdOfName, siteIdOfName = firebase.getCompaniesData()
+    notifylist = {}
     for infectedMember in infectedList:
-        infectedMember['siteName'] = siteIdOfName[infectedMember['siteId']]
-        infectedMember['companyName'] = companyIdOfName[infectedMember['companyId']]
+        infectedMember['siteName'] = firebase.getSiteData(siteId=infectedMember['siteId'], companyId=companyId)["name"]
+        infectedMember['companyName'] = firebase.getCompaniesData(infectedMember['companyId'])["name"]
+        infectedMemberlineId = firebase.getMemberData(memberId=infectedMember['memberId'])["lineId"]
         infectedMember['timestamp'] = str(
             datetime.utcfromtimestamp(infectedMember['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
+        if infectedMember["name"] not in notifylist.keys():
+            notifylist[infectedMember["name"]] = {"content": "", "lineId": infectedMemberlineId}
+        notifylist[infectedMember["name"]]["content"] += f"\n\n感染地點: {infectedMember['siteName']}\n" \
+                                                         f"感染時間: {infectedMember['timestamp']}"
+
+    for member in notifylist.keys():
         notificationModel = {
-            "receiverLineIdList": infectedMember['lineId'],
-            "messages": [
-                {
-                    "messageType": "textTemplate",
-                    "content": f"{infectedMember['name']}，您已遭感染，請盡速就醫！\n"
-                               f"感染地點: {infectedMember['siteName']}\n"
-                               f"感染時間: {infectedMember['timestamp']}"
-                }
-            ]
+            "lineId": notifylist[member]['lineId'],
+            "messageType": "textTemplate",
+            "content": f"{member}，您已遭感染，請盡速就醫！"
+                       + notifylist[member]['content']
         }
+
         notificationThread = threading.Thread(target=push.pushMessage, args=(notificationModel,))
         notificationThread.start()
     result = {
         'spreadStrength': str(request.values['spreadStrength']),
         'eventId': eventId,
         "timestamp": str(request.values['confirmedTime']).replace("T", " "),
+        "amount": len(set(pd.DataFrame(infectedList)["memberId"].drop_duplicates())),
         "name": memberData['name']
     }
 
     return render_template('quarantineList.html', infectedList=infectedList, result=result, title="疫情調查")
+    
 
-
-# ----------------------------組織管理-----------------------------
-@app.route("/organizationManagement", methods=['GET'])
-def organizationManagement():
-    companyIdOfName, siteIdOfName = firebase.getCompaniesData()
-    return render_template('organizationManagement.html', companyData=companyIdOfName, title='組織管理')
-
-
-# -----------------------getOtherCompanyData----------------------
-@app.route("/getCompanyData", methods=['POST'])
-def getCompanyData():
-    companyId = request.get_json()["companyId"]
-    companyData = {}
-    sitesData = Firebase(companyId).getSiteData()
-    companyData["sitesData"] = sitesData
-    membersData = Firebase(companyId).getMembersDataOfCompany()
-    companyData["membersData"] = membersData
-
-    return companyData
+# ----------------------------data studio-----------------------------
+@app.route("/epidemicReport", methods=['GET'])
+def getReport():
+    return render_template('dataStudio.html')
