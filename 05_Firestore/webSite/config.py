ae6e6720f263eeb9441f733392d2ea9b3b874908
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-22 13:46:16 +0800

    Update config.py

--- 05_Firestore/webSite/config.py
+++ 05_Firestore/webSite/config.py
@@ -2,7 +2,6 @@ class Config():
     def __init__(self) :
         self.channelAccessToken = '[your channelAccessToken]'
         self.__firebase_keyFile = '[your firebaseKey.json]'
-        self.__pubSub_keyFile = '[your pubsubKey.json]'
         self.companyId = '[your companyId]' 
         
     def firebase(self):
