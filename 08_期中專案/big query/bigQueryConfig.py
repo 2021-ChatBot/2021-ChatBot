86503b35ab44e2150e887041db87f64f1b30dbd7
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-24 09:21:53 +0800

    Delete 08_期中專案 directory

--- 08_期中專案/big query/bigQueryConfig.py
+++
@@ -1,9 +0,0 @@
-# BigQuery Client
-keyFile = '[your bigQueryKey.json]'
-projectId = '[your projectId]'
-
-# Bigquery dataset
-datasetId = '[your datasetId]'
-memberTableId = 'memberTable'
-footprintTableId = 'footprintTable'
-infectedTableId = 'infectedTable'
