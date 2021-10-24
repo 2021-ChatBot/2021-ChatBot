34a0bb004133a360faa1628b4855547bef87a37f
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-24 15:37:52 +0800

    Update richMenu_handler.py

--- 08_期中專案/webhook/richMenu_handler.py
+++ 08_期中專案/webhook/richMenu_handler.py
@@ -23,7 +23,10 @@ def create_richMenu(lineId, linebotapi, timeflag=True, memberId=""):
                 if area["action"]["uri"] == "/myCompany":
                     area["action"]["uri"] = WebUrl + area["action"]["uri"]
                 else:
-                    area["action"]["uri"] = WebUrl + area["action"]["uri"] + memberId
+                    if area["action"]["uri"] == "/myCompany":
+                        area["action"]["uri"] = WebUrl + area["action"]["uri"]
+                    else:
+                        area["action"]["uri"] = WebUrl + area["action"]["uri"] + memberId
 
     createResponse = requests.post(
         'https://api.line.me/v2/bot/richmenu',
