ffe42d32fb4bf5db2fed368e85e627997a37d51f
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-25 16:48:27 +0800

    Update check.py

--- 04_Flask AppEngine/webSite/check.py
+++ 04_Flask AppEngine/webSite/check.py
@@ -1,28 +1,18 @@
 from firestoreDAO import Firestore
 
-
 class CheckFootprints:
     def __init__(self, event):
         self.__firestore = Firestore()
         self.infectedFootprints = []
         footprint = self.__firestore.getFootprints(event)[0]
-        footprint['strength'] = event['strength']
-        self.check(footprint)
+        self.check(footprint, event['strength'])
         event['infectedFootprints'] = self.infectedFootprints
         self.__firestore.setEvent(event)
 
-    def check(self, data):
-        if data['strength'] == 1:
-            for memberfootprint in self.__firestore.getmemberFootprints(data):
-                if memberfootprint['timestamp'] >= data['timestamp']:
-                    memberfootprint.pop('strength', None)
-                    self.infectedFootprints.append(memberfootprint)
-        else:
-            mystrength = data['strength']
-            for footprint in self.__firestore.getsiteFootprints(data):
-                if mystrength >= 1 :
-                    footprint.pop('strength', None)
-                    self.infectedFootprints.append(footprint)
-                    footprint['strength'] = data['strength'] - 1
-                    self.check(footprint)
-                    mystrength -= 1
+
+    def check(self, footprint, mystrength):
+        if mystrength < 1 or len(footprint) == 0:
+            return
+        self.infectedFootprints.append(footprint)
+        self.check(self.__firestore.getsiteFootprint(footprint), mystrength-1)
+        self.check(self.__firestore.getmemberFootprint(footprint), mystrength)
