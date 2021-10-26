98f6e3575b353671214e6db915fb02b40156ca61
t110598002 <91366432+t110598002@users.noreply.github.com>
2021-10-26 12:40:49 +0800

    Update richMenu_handler.py

--- 04_Flask AppEngine/webhook/richMenu_handler.py
+++ 04_Flask AppEngine/webhook/richMenu_handler.py
@@ -23,10 +23,7 @@ def create_richMenu(lineId, linebotapi, timeflag=True, memberId=""):
                 if area["action"]["uri"] == "/myCompany":
                     area["action"]["uri"] = WebUrl + area["action"]["uri"]
                 else :
-                    if area["action"]["uri"] == "/myCompany":
-                        area["action"]["uri"] = WebUrl + area["action"]["uri"]
-                    else:
-                        area["action"]["uri"] = WebUrl + area["action"]["uri"] + memberId
+                    area["action"]["uri"] = WebUrl + area["action"]["uri"] + memberId
 
     createResponse = requests.post(
         'https://api.line.me/v2/bot/richmenu',
