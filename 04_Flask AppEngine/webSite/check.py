5f258226d04f2924477ad54c571f6d69eeb06eb3
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-29 11:28:49 +0800

    Update check.py

--- 04_Flask AppEngine/webSite/check.py
+++ 04_Flask AppEngine/webSite/check.py
@@ -3,17 +3,17 @@ from firestoreDAO import Firestore
 class CheckFootprints:
     def __init__(self, event):
         self.__firestore = Firestore()
-        self.infectedFootprints = []
-        footprint = self.__firestore.getFootprints(event)
-        if len(footprint) is not 0:
-            self.check(footprint[0], event['strength'])
-        event['infectedFootprints'] = self.infectedFootprints
+        event['infectedFootprints'] = []
+        footprints = self.__firestore.getFootprints(event)
+        if len(footprints) is not 0:
+            self.check(footprints[0], event['strength'], event['infectedFootprints'])
         self.__firestore.setEvent(event)
 
 
-    def check(self, footprint, mystrength):
-        if mystrength < 1 or len(footprint) == 0:
-            return
-        self.infectedFootprints.append(footprint)
-        self.check(self.__firestore.getsiteFootprint(footprint), mystrength-1)
-        self.check(self.__firestore.getmemberFootprint(footprint), mystrength)
+    def check(self, footprint, strength, infectedFootprints):
+        if strength > 0 and len(footprint) != 0:
+            infectedFootprints.append(footprint)
+            # 尋找該商店下一筆足跡
+            self.check(self.__firestore.getsiteFootprint(footprint), strength - 1, infectedFootprints)
+            # 尋找該成員下一筆足跡
+            self.check(self.__firestore.getmemberFootprint(footprint), strength, infectedFootprints)
