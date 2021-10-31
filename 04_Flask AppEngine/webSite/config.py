19f3b746b3c2b1eaa5dcc5701ea7f4fc50e02222
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-31 15:53:20 +0800

    Delete 04_Flask AppEngine directory

--- 04_Flask AppEngine/webSite/config.py
+++
@@ -1,8 +0,0 @@
-# ( 0 ) Line Channel
-channelAccessToken = '[your channelAccessToken]'
-
-# ( 1 ) Firestore
-keyFile = '[your firestoreKey.json]'
-companyId = '[your companyId]' 
-storageBucket = '[your storageBucket]'
-
