90cdcaff7642fe368062876eb0e4adca235b3cc7
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-10-31 10:41:28 +0800

    Update check.py

--- 04_Flask AppEngine/webSite/check.py
+++ 04_Flask AppEngine/webSite/check.py
@@ -1,19 +1,40 @@
-from firestoreDAO import Firestore
+from firebase_admin import credentials, firestore, initialize_app
+from config import keyFile, storageBucket
+
 
 class CheckFootprints:
     def __init__(self, event):
-        self.__firestore = Firestore()
-        event['infectedFootprints'] = []
-        footprints = self.__firestore.getFootprints(event)
-        if len(footprints) is not 0:
-            self.check(footprints[0], event['strength'], event['infectedFootprints'])
-        self.__firestore.setEvent(event)
+        cred = credentials.Certificate(keyFile)
+        initialize_app(cred, {'storageBucket': storageBucket})
+        self.__db = firestore.client()
+        infected = {
+            'companyId': event['companyId'],
+            'siteId': event['siteId'],
+            'memberId': 0,
+            'infectedTime': event['infectedTime'],
+            'strength': event['strength'],
+            'infectedFootprints' : []
+        }
+        try:
+            self.check(infected)
+        except IndexError:
+            pass
+        event['infectedFootprints'] = infected['infectedFootprints']
 
-
-    def check(self, footprint, strength, infectedFootprints):
-        if strength > 0 and len(footprint) != 0:
-            infectedFootprints.append(footprint)
-            # 尋找該商店下一筆足跡
-            self.check(self.__firestore.getsiteFootprint(footprint), strength - 1, infectedFootprints)
-            # 尋找該成員下一筆足跡
-            self.check(self.__firestore.getmemberFootprint(footprint), strength, infectedFootprints)
+    def check(self, infected):
+        if infected['strength'] > 0:
+            if infected['siteId'] != 0:
+                footprints_ref = self.__db.collection(f"companies/{infected['companyId']}/sites/{infected['siteId']}/footprints")
+                docs = footprints_ref.order_by(u'timestamp').where("timestamp", u">", infected["infectedTime"]).limit(infected['strength']).get()
+                footprints = [doc._data for doc in docs]
+                infected['siteId'] = 0
+                infected['memberId'] = footprints[0]['memberId']
+            else:
+                footprints_ref = self.__db.collection(f"members/{infected['memberId']}/footprints")
+                docs = footprints_ref.order_by(u'timestamp').where("timestamp", u">", infected["infectedTime"]).limit(infected['strength']).get()
+                footprints = [doc._data for doc in docs]
+                infected['siteId'] = footprints[0]['siteId']
+            infected['infectedTime'] = footprints[0]['timestamp']
+            infected['strength'] -= 1
+            infected['infectedFootprints'].extend(footprints)
+            self.check(infected)
