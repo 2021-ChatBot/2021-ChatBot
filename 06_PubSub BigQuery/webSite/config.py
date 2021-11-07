fe024cc2152dd15ef873c360f426509d68096d85
john <87940810+john920psalm9114@users.noreply.github.com>
2021-11-07 16:40:47 +0800

    Update config.py

--- 06_PubSub BigQuery/webSite/config.py
+++ 06_PubSub BigQuery/webSite/config.py
@@ -1,12 +1,14 @@
 # ( 0 ) Firestore
 companyId = '[your company id]' 
 
-
 # ( 1 ) PubSub
-projectId = 'your project Id'
-topicId = 'your topic Id'
-
+projectId = '[your project Id]'
+topicId = '[your topic Id]'
 
 # ( 2 ) Line Channel
 secretResourceId = '[your secret resource id]'
 version = '1' # 若secret沒有更新的情況下，version為1
+
+# ( 3 ) Data Studio report URL
+reportForCustomer = "[your report url for customer]"
+reportForManager = "[your report url for customer]"
