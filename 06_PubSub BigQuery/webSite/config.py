802618e69ea1f31bf5d62bb40dbd1f8e72c0c7c6
john <87940810+john920psalm9114@users.noreply.github.com>
2021-11-12 11:11:43 +0800

    Update config.py

--- 06_PubSub BigQuery/webSite/config.py
+++ 06_PubSub BigQuery/webSite/config.py
@@ -11,4 +11,4 @@ version = '1' # 若secret沒有更新的情況下，version為1
 
 # ( 3 ) Data Studio report URL
 reportForCustomer = "[your report url for customer]"
-reportForManager = "[your report url for customer]"
+reportForManager = "[your report url for manager]"
