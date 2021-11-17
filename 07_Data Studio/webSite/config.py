e1eca670549042d65e4fc277c1ed02686235876e
Arthur <43879780+zawarudooo@users.noreply.github.com>
2021-11-17 18:55:09 +0800

    Update config.py

--- 07_Data Studio/webSite/config.py
+++ 07_Data Studio/webSite/config.py
@@ -4,14 +4,12 @@ companyId = '[your company id]'
 # ( 1 ) PubSub
 projectId = '[your project Id]'
 topicId = '[your topic Id]'
+pub_key = '[your pubsubKey]'
 
-# ( 2 ) local pubsubkey
-pub_key = 'your pubsubKey'
-
-# ( 3 ) Line Channel
+# ( 2 ) Line Channel
 secretResourceId = '[your secret resource id]'
 version = '1' # 若secret沒有更新的情況下，version為1
 
-# ( 4 ) Data Studio report URL
+# ( 3 ) Data Studio report URL
 reportForCustomer = "[your report url for customer]"
 reportForManager = "[your report url for customer]"
