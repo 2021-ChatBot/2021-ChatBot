c12da83b9f35aacd25bf4088275f14e6bbec146e
Cheng Chia Shih <43398805+CCShihfromTW@users.noreply.github.com>
2021-12-18 13:59:51 +0800

    Add files via upload

---
+++ 10_Identity Platform/webSite/lineAPI.py
@@ -0,0 +1,13 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from config import channelAccessToken
+
+
+class PushMessage:
+    def __init__(self, channelToken=channelAccessToken):
+        self.lineBotApi = LineBotApi(channelToken)
+
+    def pushMessage(self, inputModel):
+        if inputModel['messageType'] == 'textTemplate':
+            templateMessage = TextSendMessage(text=inputModel['content'])
+            self.lineBotApi.push_message(inputModel['lineId'], [templateMessage])
---
+++ 10_Identity Platform/webSite/main.py
@@ -0,0 +1,374 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import json
+import time
+import uuid
+from datetime import datetime, timedelta
+import threading
+from copy import deepcopy
+from config import *
+from checker import check
+from lineAPI import PushMessage
+from apiClient import requestAPI
+from publish import publish_messages
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask.json import JSONEncoder
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import (
+    Security,
+    RoleMixin,
+    UserMixin,
+    SQLAlchemyUserDatastore,
+    login_required,
+    auth_required,
+    current_user,
+    hash_password,
+    roles_accepted,
+    logout_user,
+)
+
+line = PushMessage()
+
+# Cloud SQL
+url = 'mysql+pymysql://' + USER_NAME + ':' + PASSWORD + '@localhost/' + DBNAME + '?unix_socket=/cloudsql/' + CONNECT_NAME
+
+# Create app
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+for i in flaskConfig:
+    app.config[i] = flaskConfig[i]
+app.config["SQLALCHEMY_DATABASE_URI"] = url
+app.json_encoder = JSONEncoder
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+# Define models
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    lineId = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+# Setup Flask-Security
+userDatabase = SQLAlchemyUserDatastore(db, User, Role)
+app.security = Security(app, userDatabase)
+
+
+# ----------------------------------------------------------------------
+def extraction(user):
+    member = {}
+    attrs = ("id", "email", "name", "lineId")
+    for attr in attrs:
+        member[attr] = getattr(user, attr)
+    member["role"] = user.roles[0].name
+    return member
+
+
+# ----------------------成員註冊-----------------------------------------
+@app.route("/signUp", methods=['GET', 'POST'])
+def signUp():
+    if request.method == 'GET':
+        return render_template("signUp.html", liffId=liffId)
+
+    elif request.method == 'POST':
+        userData = request.form.to_dict()
+        emailQuery = User.query.filter_by(email=userData['email']).first()
+        lineQuery = User.query.filter_by(lineId=userData['lineId']).first()
+
+        if emailQuery == None:
+            if userData['lineId'] == "" or lineQuery == None:
+                user = userDatabase.create_user(
+                    id=str(uuid.uuid4()), email=userData['email'], password=hash_password(userData['password']),
+                    name=userData['name'], lineId=userData['lineId'], roles=["customer"]
+                )
+                db.session.commit()
+                member = extraction(user)
+
+                # - pubsub                
+                member["companyName"] = requestAPI("GET", "/company/" + companyId)['name']
+                publishThread = threading.Thread(target=publish_messages, args=({"member": member},))
+                publishThread.start()
+                if userData['lineId'] == "":
+                    return render_template("signUpSucessNotBind.html", member=member, channel_href=channel_href, title="已註冊未綁定")
+                else:
+                    message = {
+                        "lineId": member['lineId'],
+                        "messageType": "textTemplate",
+                        "content": "註冊綁定成功\n"
+                                   f"name: {member['name']}\n"
+                                   f"email: {member['email']}\n"
+                                   f"lineId: {member['lineId']}"
+                    }
+                    notificationThread = threading.Thread(target=line.pushMessage, args=(message,))
+                    notificationThread.start()
+
+                    return render_template("signUpSucess.html", member=member, title="註冊成功")
+        elif lineQuery != None:
+            return render_template("signUpFail.html", title="註冊失敗 您已經重複註冊")
+        else:
+            return render_template("signUpFail.html", title="註冊失敗 本信箱(" + str(emailQuery.email) + ")已由他人註冊")
+
+
+# ----------------------個人資料-----------------------------------------
+@app.route("/myData", methods=['GET', 'POST'])
+@auth_required()
+@roles_accepted("customer", "manager", "admin")
+def myData():
+    member = extraction(current_user)
+    if request.method == 'GET':
+        return render_template('myData.html', member=member, title="我的個資")
+    elif request.method == 'POST':
+        userData = request.form.to_dict()
+        current_user.name = userData['name']
+        if member['role'] != userData['role']:
+            userDatabase.remove_role_from_user(current_user, member['role'])
+            userDatabase.add_role_to_user(current_user, userData['role'])
+        db.session.commit()
+        return redirect("/myData")
+
+    # ----------------------修改個資-----------------------------------------
+
+
+@app.route("/myForm", methods=['GET'])
+@auth_required()
+@roles_accepted("customer", "manager", "admin")
+def myForm():
+    member = extraction(current_user)
+    return render_template('myForm.html', member=member, title="修改個資")
+
+
+# ----------------------掃碼---------------------------------------------
+@app.route("/newFootprint", methods=['GET'])
+@auth_required()
+@roles_accepted("customer")
+def newFootprint():
+    return render_template('newFootprint.html', title="實聯掃碼")
+
+
+# ----------------------寫入掃碼足跡紀錄-----------------------------------
+@app.route("/newFootprint", methods=['POST'])
+@auth_required()
+@roles_accepted("customer")
+def setMyFootprint():
+    footprintData = json.loads(request.get_data())
+    siteIdRegex = re.compile(r'\d\d\d\d \d\d\d\d \d\d\d\d \d\d\d')
+    member = extraction(current_user)
+    try:
+        siteId = siteIdRegex.findall(footprintData['siteData'])[0]
+        importTime = int(time.time() + 28800)
+    except:
+        return jsonify('這不是實聯制QRcode')
+    site = requestAPI("GET", "/site/" + companyId + "/" + siteId)
+    if site != None:
+        message = {
+            "lineId": member['lineId'],
+            "messageType": "textTemplate",
+            "content": "掃碼成功\n"
+                       f"商店: {site['name']}\n"
+                       f"時間: {str(datetime.utcfromtimestamp(importTime).strftime('%Y-%m-%d %H:%M:%S'))}"
+        }
+        notificationThread = threading.Thread(target=line.pushMessage, args=(message,))
+        notificationThread.start()
+
+        # - firestore
+        footprint = {
+            'memberId': member['id'],
+            'siteId': siteId,
+            'companyId': companyId,
+            'timestamp': importTime
+        }
+        footprint['id'] = requestAPI("POST", "/footprint", footprint)['id']
+
+        # - pubsub
+        footprint["companyName"] = requestAPI("GET", "/company/" + companyId)['name']
+        publishThread = threading.Thread(target=publish_messages, args=({'footprint': footprint},))
+        publishThread.start()
+        return jsonify(site['name'] + '  到店掃碼成功')
+
+    return jsonify('這不是實聯制QRcode')
+
+
+# ----------------------------User掃碼足跡紀錄-----------------------------
+@app.route("/myFootprints", methods=['GET'])
+@auth_required()
+@roles_accepted("customer")
+def myFootprints():
+    member = extraction(current_user)
+    companyName = requestAPI("GET", "/company/" + companyId)['name']
+    footprints = requestAPI("GET", "/footprint/" + companyId + "/" + member['id'])
+    for footprint in footprints:
+        footprint['companyName'] = companyName
+        footprint['siteName'] = requestAPI("GET", "/site/" + companyId + "/" + footprint['siteId'])['name']
+        footprint['timestamp'] = str(datetime.utcfromtimestamp(footprint['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
+
+    return render_template('myFootprints.html', footprints=footprints, title="足跡列表")
+
+
+# ----------------------------我的企業-----------------------------
+@app.route("/myCompany", methods=['GET'])
+@auth_required()
+@roles_accepted("manager")
+def myCompany():
+    company = requestAPI("GET", "/company/" + companyId)
+    sites = requestAPI("GET", "/site/" + companyId)
+    users = User.query.filter().all()
+    members = []
+    for user in users:
+        member = extraction(user)
+        members.append(member)
+    result = {
+        'sites': sites,
+        'members': members,
+        'company': company
+    }
+    return render_template('myCompany.html', result=result, title='我的企業')
+
+
+# ----------------------------增修商店------------------------------------
+@app.route("/mySite", methods=['GET'])
+@app.route("/mySite/<siteId>", methods=['GET'])
+@auth_required()
+@roles_accepted("manager")
+def mySite(siteId=None):
+    company = requestAPI("GET", "/company/" + companyId)
+    if siteId == None:
+        site = None
+    else:
+        site = requestAPI("GET", "/site/" + companyId + "/" + siteId)
+
+    result = {
+        'company': company,
+        'site': site,
+    }
+    return render_template('mySite.html', result=result, title='增修商店')
+
+
+# ----------------------------增修商店------------------------------------
+@app.route("/mySite", methods=['POST'])
+@auth_required()
+@roles_accepted("manager")
+def newSite():
+    site = request.form.to_dict()
+    site['companyId'] = companyId
+    exist = requestAPI("GET", "/site/" + companyId + "/" + site['id'])
+    if exist == None:
+        # - pubsub
+        site['companyName'] = requestAPI("GET", "/company/" + companyId)['name']
+        publishThread = threading.Thread(target=publish_messages, args=({'site': site},))
+        publishThread.start()
+        del site['companyName']
+
+    requestAPI("POST", "/site", site)
+    return redirect(url_for('myCompany'))
+
+
+# ----------------------------疫情調查設定-----------------------------
+@app.route("/checkFootprints", methods=['GET'])
+@auth_required()
+@roles_accepted("admin")
+def checkFootprints():
+    companyName = requestAPI("GET", "/company/" + companyId)['name']
+    sites = requestAPI("GET", "/site/" + companyId)
+    return render_template('checkFootprints.html', sites=sites, companyName=companyName, title="疫情調查")
+
+
+# ----------------------------疫情調查結果-----------------------------
+@app.route("/infectedFootprints", methods=['POST'])
+@auth_required()
+@roles_accepted("admin")
+def infectedFootprints():
+    event = request.form.to_dict()
+    event['companyId'] = companyId
+    event['strength'] = int(event['strength'])
+    event['infectedTime'] = int(time.mktime(datetime.strptime(request.values['infectedTime'], "%Y-%m-%dT%H:%M:%S").timetuple()))
+
+    infected = deepcopy(event)
+    infected['memberId'] = 0
+    infected['myStrength'] = event['strength']
+    infected['infectedFootprints'] = []
+
+    check(infected)
+    event['infectedFootprints'] = infected['infectedFootprints']
+    event['eventId'] = requestAPI("POST", "/event", event)['eventId']
+
+    # line push message------------------------------------------------------
+    infectedText = {}
+    companyName = requestAPI("GET", "/company/" + companyId)['name']
+    for footprint in event['infectedFootprints']:
+        # - pubsub
+        infectedFootprint = {
+            "eventId": event['eventId'],
+            "companyName": companyName,
+            "siteId": footprint["siteId"],
+            "memberId": footprint["memberId"],
+            "footprintId": footprint["id"],
+            "strength": event["strength"],
+            "eventTimestamp": event["infectedTime"],
+            "footprintTimestamp": footprint["timestamp"]
+        }
+        publishThread = threading.Thread(target=publish_messages, args=({'infected': infectedFootprint},))
+        publishThread.start()
+
+        # ---
+        footprint['siteName'] = requestAPI("GET", "/site/" + companyId + "/" + footprint['siteId'])['name']
+        footprint['companyName'] = companyName
+        # SQL get member name
+        footprint['memberName'] = User.query.filter_by(id=footprint['memberId']).first().name
+        footprint['infectedTime'] = str(datetime.utcfromtimestamp(footprint['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
+
+        if footprint['memberName'] not in infectedText.keys():
+            # SQL get member lineId
+            lineId = User.query.filter_by(id=footprint['memberId']).first().lineId
+            infectedText[footprint['memberName']] = {"content": "", "lineId": lineId}
+        infectedText[footprint['memberName']]["content"] += f"\n\n感染地點: {footprint['siteName']}\n感染時間: {footprint['infectedTime']}"
+
+    for memberName in infectedText.keys():
+        message = {
+            "lineId": infectedText[memberName]['lineId'],
+            "messageType": "textTemplate",
+            "content": f"{memberName}，您已遭感染，請盡速就醫！" + infectedText[memberName]['content']
+        }
+        notificationThread = threading.Thread(target=line.pushMessage, args=(message,))
+        notificationThread.start()
+    # -----------------------------------------------------------------------
+    result = {
+        'eventId': event['eventId'],
+        'strength': str(request.values['strength']),
+        "infectedTime": str(request.values['infectedTime']).replace("T", " "),
+        "amount": len(set([member['memberId'] for member in event['infectedFootprints']])),
+        "siteName": requestAPI("GET", "/site/" + companyId + "/" + event['siteId'])["name"],
+        "footprints": sorted(event['infectedFootprints'], key=lambda i: (i['memberName'], i['timestamp']))
+    }
+
+    return render_template('infectedFootprints.html', result=result, title="疫情調查")
+
+
+# ----------------------------data studio-----------------------------
+@app.route("/report", methods=['GET'])
+@auth_required()
+@roles_accepted("admin")
+def myReport():
+    return render_template('dataStudio.html', report=report)
+
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(threaded=True, host='127.0.0.1', port=port)
