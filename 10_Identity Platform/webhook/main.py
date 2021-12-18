ba1aad1bf6381971afc6ae9d80687e3b7489d69b
john <87940810+john920psalm9114@users.noreply.github.com>
2021-12-19 01:19:20 +0800

    Update main.py

--- 10_Identity Platform/webhook/main.py
+++ 10_Identity Platform/webhook/main.py
@@ -3,31 +3,50 @@ import cloudSqlClient as cloudSql
 from linebot import WebhookHandler, LineBotApi
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import FollowEvent, TextSendMessage, MessageEvent, TextMessage, TemplateSendMessage, URITemplateAction, CarouselTemplate, CarouselColumn
-from config import channelSecret, channelAccessToken, LiffUrl
+from config import channelSecret, channelAccessToken, signUpUrl, bindingUrl
 
 
 # （0） Messages
 welcomeMessage = TextSendMessage(text = '歡迎加入 < 智能防疫社群 > ')
 backMessage = TextSendMessage(text = '你已經完成註冊綁定\n歡迎回到 < 智能防疫社群 > ')
-bindHandleMessage = TextSendMessage(text = '正在為你綁定')
-bindSuccessText = '已為你完成綁定\n'
 registerMessage = TemplateSendMessage(
-        alt_text='以下有新訊息... ',
-        template=CarouselTemplate(
-            columns=[
-                CarouselColumn(
-                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/McDonald%27s_logo.svg/1200px-McDonald%27s_logo.svg.png',
-                    text='麥當勞會員系統',
-                    actions=[
-                        URITemplateAction(
-                            label='註冊',
-                            uri=LiffUrl
-                        ),
-                    ]
-                )
-            ]
-        )
+    alt_text='以下有新訊息... ',
+    template=CarouselTemplate(
+        columns=[
+            CarouselColumn(
+                thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/McDonald%27s_logo.svg/1200px-McDonald%27s_logo.svg.png',
+                title='麥當勞會員系統',
+                text='你是新顧客，請註冊',
+                actions=[
+                    URITemplateAction(
+                        label='註冊',
+                        uri=signUpUrl
+                    ),
+                ]
+            )
+        ]
+    )
 )
+
+bindingMessage = TemplateSendMessage(
+    alt_text='以下有新訊息... ',
+    template=CarouselTemplate(
+        columns=[
+            CarouselColumn(
+                thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/McDonald%27s_logo.svg/1200px-McDonald%27s_logo.svg.png',
+                title='麥當勞會員系統',
+                text='你是舊會員，請綁定',
+                actions=[
+                    URITemplateAction(
+                        label='綁定',
+                        uri=bindingUrl
+                    ),
+                ]
+            )
+        ]
+    )
+)
+
 menuMessage = TextSendMessage(text='本系統提供下列功能：\n' \
                                     + '1. 實聯掃碼\n' \
                                     + '2. 我的足跡\n' \
@@ -81,16 +100,7 @@ def handle_queryResult(queryResult, lineId):
         if queryResult['parameters']['email']:
             email = queryResult['parameters']['email']
             if cloudSql.query(False, email):
-                lineBotApi.push_message(lineId, bindHandleMessage)
-                cloudSql.updateLineId(lineId, email)
-                member = cloudSql.query(lineId, email)
-                bindSuccessMessage = TextSendMessage(
-                    text = bindSuccessText + 'name='   + member['name'] + '\n' \
-                                           + 'email='  + email          + '\n' \
-                                           + 'lineId=' + lineId
-                )
-                lineBotApi.push_message(lineId, bindSuccessMessage)
-                lineBotApi.push_message(lineId, menuMessage)
+                lineBotApi.push_message(lineId, bindingMessage)
             else:
                 lineBotApi.push_message(lineId, registerMessage)
 
