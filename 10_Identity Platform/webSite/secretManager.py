3fb79fab57dafdbbd90d8b3d222372fb9ccf8f49
t110598002 <91366432+t110598002@users.noreply.github.com>
2021-12-19 09:25:12 +0800

    Delete 10_Identity Platform directory

--- 10_Identity Platform/webSite/secretManager.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import secretmanager
-
-
-def access_secret_version(resourceId, version):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"{resourceId}/versions/{version}"
-    response = client.access_secret_version(request={"name": name})
-    payload = response.payload.data.decode("UTF-8")
-    return payload
